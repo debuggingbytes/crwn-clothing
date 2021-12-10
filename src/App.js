f01c78e8d14c33e80bcfa4750eb9ceb6cd3f72a0
Kris <kris@debuggingbytes.com>
2021-12-10 13:02:25 -0700

    Updated App.js with Header file, added header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={ <HomePage /> } />
         <Route path="/shop/" element={ <ShopPage /> } />
