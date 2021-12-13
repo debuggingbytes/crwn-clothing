1590444824f18b9306fa09917d0d3234ceecdb6e
Kris <kris@debuggingbytes.com>
2021-12-13 11:05:23 -0700

    Added total values to Cart, with updated array, Created CartItem Component to list all items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      {
+        cartItems.map(cartItem => <CartItem key={ cartItem.id } item={ cartItem } />)
+      }
     </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+export default connect(mapStateToProps)(Cart);
